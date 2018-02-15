9e0964e45cab1d3a6e77eb38763cd148f6cfcfdf
Jarosław Pustuła <jpustula@objectivity.co.uk>
2018-02-15 08:15:53 +0100

    Introduce environment files read during template compilation

--- tasks/build/_views.js
+++ tasks/build/_views.js
@@ -1,18 +1,30 @@
+import fs from 'fs';
 import gulp from 'gulp';
 import nunjucks from 'gulp-nunjucks-render';
+import path from 'path';
 import * as config from '../config';
+
+function readEnvironmentData() {
+  const environmentFilePath = path.join(__dirname, '../../src/environments/env.json');
+  const environmentData = JSON.parse(fs.readFileSync(environmentFilePath, 'utf8'));
+
+  return environmentData;
+}
 
 /**
  * Task: build:views
  */
 function buildViews() {
+  const compileOptions = {
+    data: readEnvironmentData(),
+    path: [
+      'src/assets/icons',
+      'src/views'
+    ]
+  };
+
   return gulp.src('src/views/pages/**/*.njk')
-    .pipe(nunjucks({
-      path: [
-        'src/assets/icons',
-        'src/views'
-      ]
-    }))
+    .pipe(nunjucks(compileOptions))
     .pipe(gulp.dest('build'));
 }
 buildViews.displayName = 'build:views';
