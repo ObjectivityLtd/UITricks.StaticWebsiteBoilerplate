8d8ceefd78fd79a3a3e3f8b036b812a7f876e512
Jarosław Pustuła <jpustula@objectivity.co.uk>
2018-02-26 15:55:46 +0100

    Add webpack like tilde alias pointing to node_modules for Sass imports

--- tasks/build/_styles.js
+++ tasks/build/_styles.js
@@ -1,13 +1,18 @@
 import gulp from 'gulp';
 import sass from 'gulp-sass';
+import tildeImporter from 'node-sass-tilde-importer';
 import * as config from '@tasks/config';
 
 /**
  * Task: build:styles
  */
 export function buildStyles() {
+  const options = {
+    importer: tildeImporter
+  };
+
   return gulp.src(`${config.paths.src}/styles/**/*.+(sass|scss)`)
-    .pipe(sass().on('error', sass.logError))
+    .pipe(sass(options).on('error', sass.logError))
     .pipe(gulp.dest(`${config.paths.dist}/styles`));
 }
 buildStyles.displayName = 'build:styles';
